797d71d901d65c64de538acd08d2ab11f0de5c7e
Jack Dewinter <jackdewinter1@gmail.com>
2024-11-09 14:06:18 +0000

    remove vercel sync env vars for now

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
-import { syncVercelEnvVars } from "@trigger.dev/build/extensions/core";
 
 export default defineConfig({
   project: "proj_astwlnkvagktjhapavpu",
-  build: {
-    extensions: [syncVercelEnvVars()],
-  },
   runtime: "node",
   logLevel: "log",
   // Set the maxDuration to 300 seconds for all tasks. See https://trigger.dev/docs/runs/max-duration
